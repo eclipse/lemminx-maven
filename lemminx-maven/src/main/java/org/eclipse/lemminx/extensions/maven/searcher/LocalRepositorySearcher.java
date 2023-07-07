31fa85252a52922563640d3457fa7fb46c813b4e
Victor Rubezhny <vrubezhny@redhat.com>
2023-07-07 02:51:35 +0200

    Fix NPE in 'LocalRepositorySearcher.getLocalArtifactsLastVersion'
    ```
    Exception in thread "Thread-61" java.lang.NullPointerException: Cannot invoke "java.nio.file.WatchService.take()" because "this.watchService" is null
    	at org.eclipse.lemminx.extensions.maven.searcher.LocalRepositorySearcher.lambda$getLocalArtifactsLastVersion$1(LocalRepositorySearcher.java:73)
    	at java.base/java.lang.Thread.run(Thread.java:833)
    ```

--- lemminx-maven/src/main/java/org/eclipse/lemminx/extensions/maven/searcher/LocalRepositorySearcher.java
+++ lemminx-maven/src/main/java/org/eclipse/lemminx/extensions/maven/searcher/LocalRepositorySearcher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019-2020 Red Hat Inc. and others.
+ * Copyright (c) 2019, 2023 Red Hat Inc. and others.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -70,7 +70,7 @@ public class LocalRepositorySearcher {
 			new Thread(() -> {
 				WatchKey key;
 				try {
-					while ((key = watchService.take()) != null) {
+					while ((key = (watchService != null ? watchService.take() : null)) != null) {
 						if (watchKey.equals(key)) {
 							cache.remove(localRepository);
 							key.reset();
